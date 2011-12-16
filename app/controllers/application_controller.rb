d490f30da6045cc344d790ac93e40116a7b3b38e
Yen Ha-Thi-Bach <yencn02@gmail.com>
2011-12-16 19:59:26 +0700

    authentication request with http basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  USER_ID, PASSWORD = "admin", "admin123"
+  before_filter :basic_authenticate
   protect_from_forgery
 
   include AuthenticatedSystem
 
+  protected
+  def basic_authenticate
+    authenticate_or_request_with_http_basic('Please enter username and password') do |id, password|
+        id == USER_ID && password == PASSWORD
+    end
+  end
 end
