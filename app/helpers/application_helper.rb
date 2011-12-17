7ebac869d36049817ade2909b618e31b454f6b21
Yen Ha-Thi-Bach <yencn02@gmail.com>
2011-12-17 16:07:25 +0700

    states by country

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-  def load_states(carmen_states)
-    state_options = {"Default" => options_for_select(["", ""])}
-    for i in 0...carmen_states.size
-      country = carmen_states[i][0]
-      states = carmen_states[i][1]
+  def states_by_country
+    state_options = {}
+    Carmen::country_codes.each do |country|
+      states = Carmen::states(country) rescue []
       state_options[country] = options_for_select(states)
     end
     javascript_tag("var states = #{state_options.to_json}")
