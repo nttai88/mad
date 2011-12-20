510de3da8dc86889ef3f358126ae7e60e60443d0
Yen Ha-Thi-Bach <yencn02@gmail.com>
2011-12-20 11:53:50 +0700

    load routing after init

--- config/application.rb
+++ config/application.rb
@@ -56,9 +56,9 @@ module Mad2
       Bundler.require(:default, :assets, Rails.env)
     end
 
-    initializer 'add named route overrides' do |app|
+
+    config.after_initialize do |app|
       app.routes_reloader.paths << File.expand_path('../named_routes_overrides.rb',__FILE__)
-      #this seems to cause these extra routes to be loaded last, so they will define named routes last.
     end
 
   end
