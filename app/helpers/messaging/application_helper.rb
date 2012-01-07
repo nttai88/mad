8a6670d7acd87961c2d24527c4cd7826ca33bdf3
Yen Ha Thi Bach <yencn02@gmail.com>
2012-01-07 16:51:00 +0700

    Change layout for /messaging

---
+++ app/helpers/messaging/application_helper.rb
@@ -0,0 +1,10 @@
+module Messaging
+  module ApplicationHelper
+    include Refinery::SiteBarHelper
+    include Refinery::MenuHelper
+    include Refinery::MetaHelper
+    include Refinery::HtmlTruncationHelper
+    include Refinery::ImageHelper
+    include Refinery::PaginationHelper
+  end
+end
