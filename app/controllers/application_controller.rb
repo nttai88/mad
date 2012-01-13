d02804e325a8a07e47012cad758f35f8ce725d73
Yen Ha Thi Bach <yencn02@gmail.com>
2012-01-13 15:58:47 +0700

    Use https for user-pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < ActionController::Base
   private
 
   def force_ssl
-    puts SECURE_CONTROLLERS.inspect, controller_name
     if SECURE_CONTROLLERS.include?(controller_name) && !request.ssl?# && Rails.env == 'production'
       redirect_to request.url.gsub(/^http:/, "https:")
       return false;
@@ -27,20 +26,13 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options)
     defaults = {}
-
-    if USE_EXPLICIT_HOST_IN_ALL_LINKS
-      # This will OVERRIDE only_path => true, not just set the default.
-      options[:only_path] = false
-      # Now set the default protocol appropriately:
-      if actions = SECURE_ACTIONS[ (options[:controller] || controller_name).to_sym ] and
-          actions.include? options[:action]
-
-        defaults[:protocol] = 'https://'
-        defaults[:host] = SECURE_SERVER if defined? SECURE_SERVER
-      else
-        defaults[:protocol] = 'http://'
-        defaults[:host] = NON_SECURE_SERVER if defined? NON_SECURE_SERVER
-      end
+    # This will OVERRIDE only_path => true, not just set the default.
+    options[:only_path] = false
+    # Now set the default protocol appropriately:
+    if SECURE_CONTROLLERS.include?(controller_name)
+      defaults[:protocol] = 'https://'
+    else
+      defaults[:protocol] = 'http://'
     end
     return defaults
   end
