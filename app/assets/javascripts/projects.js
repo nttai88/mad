d50ddbe226835a95aaacb6f3b722d7e6cf4893e3
Yen Ha Thi Bach <yencn02@gmail.com>
2012-02-17 13:51:05 +0700

    Fix bug on add link/image dialog

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,3 +2,7 @@
 //= require i18n-messages
 //= require jquery/jquery.corner
 //= require refinery/wymeditor
+
+iframed = function() {
+  return (parent && parent.document && parent.document.location.href != document.location.href && $.isFunction(parent.$));
+};
