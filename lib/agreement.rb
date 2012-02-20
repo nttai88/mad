8eacc1c3e1d85ceeb1d3919e79e1fd1bd22ecd35
Yen Ha Thi Bach <yencn02@gmail.com>
2012-02-20 15:15:31 +0700

    Change signup agreement

--- lib/agreement.rb
+++ lib/agreement.rb
@@ -3,17 +3,196 @@ class Agreement
   class << self
     def entrepreneur
       return <<-str
-System's Entrepreneur Agreement
+KONFIDENSIALITETSERKLÆRING
+Denne avtalen om konfidensialitet er inngått mellom partene idéhaver & The MadLab Institute of
+Innovation (MadLab).
+I forbindelse med deling av informasjon mellom partene for avklaring av idéhaver’s behov vil MadLab
+motta informasjon om forretningsplan, ide, tegninger, tekniske løsninger og annen relevant
+informasjon som er konfidensiell.
+Som et vilkår for å motta informasjonen forplikter MadLab seg til å overholde følgende
+bestemmelser:
+i)
+ii)
+iii)
+MadLab skal behandle all informasjon strengt konfidensielt og skal ikke overgi noen
+informasjon til eventuell tredjepart med mindre det foreligger en lovpålagt plikt til dette.
+MadLab skal ikke benytte informasjonen, verken direkte eller indirekte for annet formål
+enn å utføre et oppdrag.
+Ved mottak av skriftlig påkrav fra Idéhaver skal MadLab umiddelbart tilbakelevere all
+skriftlig informasjon mottatt av Idéhaver i henhold til konfidensialitetserklæringen.
+Foregående konfidensialitetsforpliktelser skal ikke gjelde dersom informasjon som
+i)
+ii)
+iii)
+Er eller blir offentliggjort uten at dette skyldes at MadLab har begått brudd på denne
+konfidensialitetserklæringen.
+MadLab kan dokumentere å ha hatt i sin besittelse før den ble overlevert fra idéhaver
+eller,
+MadLab kan dokumentere ble gitt til MadLab av en tredjepart uten at dette var i strid
+med forpliktelsene tredjeparten hadde ovenfor Idéhaver.
+The MadLab Institute of Innovation garanterer at samtlige ansatte, samt personell engasjert i
+MadLab er underlagt taushetsplikt som omhandlet denne konfidensialitetserklæringen.
+Denne konfidensialitetserklæringen skal være undergitt norsk rett. MadLab vedtar Oslo tingrett som
+ikke-ekslusiv verneting i forbindelse med eventuelle tvister i tilknytning til denne
+konfidensialitetserklæringen.
+Taushetserklæring
+Jeg forplikter meg herved til ikke å bruke, åpenbare, utlevere eller på annen måte
+gjøre tilgjengelig for uvedkommende informasjon om data og bedrifts- eller
+forretningshemmeligheter, personopplysninger, eller bedrifts- eller forretningsmessig
+know-how som jeg har fått kjennskap til i mitt arbeid ved bedriften.
+Jeg vil også vise aktsomhet i omtale av andre forhold som jeg blir kjent med eller
+erfarer under mitt arbeid og samarbeid med MadLab.
+Jeg har lest de lovbestemmelser som er anført på de neste sidene av denne
+erklæring.
+Jeg er dermed klar over straffelovens §145, §294 og §405a samt
+markedsføringslovens §7 og §8. Brudd på disse bestemmelsene kan medføre
+straffeansvar.
+Jeg er også klar over at denne taushetserklæring gjelder etter opphør av
+Samarbeidsforholdet med MadLab i henhold til lovene referert i avsnittet ovenfor.
+Taushetserklæring Side 2 av 3
+AVSKRIFT AV DE VIKTIGSTE LOVBESTEMMELSER SOM VIL KUNNE
+KOMME TIL ANVENDELSE VED UBERETTIGET BRUK OG ÅPENBARING AV
+DATA og BEDRIFTSHEMMELIGHETER
+Lov nr 10 av 22. mai 1902: Almindelig borgerlig Straffelov (Straffeloven).
+§ 294. Med Bøder eller Fængsel indtil 6 Maaneder straffes den, som
+1. ved at fremkalde eller styrke en Vildfarelse retsstridig forleder nogen til en Handling,
+hvorved der voldes denne eller nogen, paa hvis Vegne han handler, Formuestab, eller som
+medvirker hertil, eller
+2. uberettiget enten selv gjør Brug af en Forretnings- eller Driftshemmelighed
+vedkommende en Bedrift, hvori han er eller i Løbet af de 2 sidste Aar har været ansat,
+eller hvori han har eller i Løbet af de 2 sidste Aar har havt Del, eller aabenbarer en saadan
+i Hensigt at sætte en anden i stand til at gjøre Brug af den, eller som ved Forledelse eller
+Tilskyndelse medvirker hertil, eller
+3. uberettiget gjør bruk av en bedrifts forretnings- eller bedriftshemmelighet som han har
+fått kjennskap til eller rådighet over i egenskap av teknisk eller merkantil konsulent for
+bedriften eller i anledning et oppdrag fra den, eller uberettiget åpenbarer en slik
+hemmelighet i den hensikt å sette andre i stand til å gjøre bruk av den, eller som ved
+forledelse eller tilskynding medvirker til dette.
+Offentlig Paatale finder alene Sted, naar det begjæres af fornærmede og findes paakrævet af
+almene Hensyn.
+§ 405a. Med bøter eller fengsel inntil 3 måneder straffes den som på urimelig måte skaffer
+seg eller søker å skaffe seg kunnskap om eller rådighet over en bedriftshemmelighet.
+LOV 1972-06-16 nr 47: Lov om kontroll med markedsføring og avtalevilkår
+(markedsføringsloven).
+§ 7. Bedriftshemmeligheter.
+Den som har fått kunnskap om eller rådighet over en bedriftshemmelighet i anledning av et
+tjeneste-, tillitsvervs- eller forretningsforhold, må ikke rettsstridig utnytte hemmeligheten i
+næringsvirksomhet.
+Det samme gjelder den som har oppnådd kunnskap om eller rådighet over en
+bedriftshemmelighet gjennom noens brudd på taushetsplikt eller gjennom noens rettsstridige
+handling ellers.
+§ 8. Tekniske hjelpemidler.
+Den som er blitt betrodd tekniske tegninger, beskrivelser, oppskrifter, modeller eller liknende
+tekniske hjelpemidler i anledning av et tjeneste-, tillitsvervs- eller forretningsforhold, må ikke
+rettsstridig utnytte dem i næringsvirksomhet.
+Det samme gjelder den som har oppnådd rådighet over tekniske tegninger, beskrivelser,
+oppskrifter, modeller og liknende tekniske hjelpemidler gjennom noens rettsstridige handling.
+Lov 2005-04-08 nr 16 Lovtiltak mot datakriminalitet (Straffeloven)
+§ 145. Den som uberettiget bryter brev eller annet lukket skrift eller på liknende måte skaffer
+seg adgang til innholdet, eller baner seg adgang til en annens låste gjemmer, straffes med
+bøter eller med fengsel inntil 6 måneder eller begge deler.
+Det samme gjelder den som uberettiget skaffer seg adgang til data eller
+programutrustning som er lagret eller som overføres ved elektroniske eller andre tekniske
+hjelpemidler.
+Voldes skade ved erverv eller bruk av slik uberettiget kunnskap, eller er forbrytelsen
+forøvet i hensikt å skaffe noen en uberettiget vinning, kan fengsel inntil 2 år anvendes.
+Medvirkning straffes på samme måte.
+Offentlig påtale finner bare sted når allmenne hensyn krever det.
+Endret ved lover 16 feb 1979 nr. 3, 12 juni 1987 nr. 54, 8 april 2005 nr. 16.
+§ 145a. Med bøter eller fengsel inntil 6 måneder straffes den som
+1. ved hjelp av hemmelig lytteapparat avlytter telefonsamtale eller annen samtale mellom
+andre, eller forhandlinger i lukket møte som han ikke selv deltar i, eller
+2. ved hjelp av lydbånd eller annen teknisk innretning hemmelig gjør opptak av samtale som
+foran nevnt eller av forhandlinger i lukket møte som han enten ikke selv deltar i, eller
+som han har skaffet seg adgang til ved falske foregivender eller ved å snike seg inn, eller
+3. anbringer lytteapparat, lydbånd eller annen teknisk innretning i øyemed som foran nevnt.
+Medvirkning straffes på samme måte.
+Offentlig påtale finner bare sted når det kreves av almene hensyn.
+Tilføyd ved lov 12 des 1958 nr. 1.
+§ 145b. Den som uberettiget gjør tilgjengelig for andre passord eller andre data som kan gi
+tilgang til et datasystem, straffes for spredning av tilgangsdata med bøter eller fengsel inntil 6
+måneder eller begge deler.
+Grov spredning av tilgangsdata straffes med fengsel inntil 2 år. Ved avgjørelsen av om
+spredningen er grov, skal det særlig legges vekt på om dataene kan gi tilgang til sensitive
+opplysninger, om spredningen er omfattende og om handlingen for øvrig skaper fare for
+betydelig skade.
+Medvirkning straffes på samme måte.
+Tilføyd ved lov 8 april 2005 nr. 16.
+
       str
     end
     def advisor
       return <<-str
-System's Advisor Agreement
+Taushetserklæring for rådgivere og nettverkspersonell tilknyttet
+The MadLab Institute of Innovation
+Jeg forstår
+- at jeg i mitt arbeid vil komme over forhold som ikke uvedkommende skal få kjennskap til.
+Jeg forplikter meg til
+- å bevare absolutt taushet overfor uvedkommende om forhold som jeg får kjennskap til ved
+mitt arbeide i gjennom The MadLab Institute of Innovation (TMII) samt behandle slik
+informasjon med forsiktighet også overfor andre medarbeidere i TMII. Dette omfatter all
+kunnskap om interne anliggender i TMII, hos TMIIs kunder og samarbeidspartnere.
+- å utvise den største varsomhet med TMIIs korrespondanse, kontrakter, avtaler m.v., slik at
+uvedkommende ikke får kjennskap til innholdet.
+Jeg er klar over
+- At ethvert brudd på taushetsplikten er oppsigelsesgrunn for denne avtale.
+- At taushetsplikten, med de nevnte begrensningene, også gjelder etter at jeg har avsluttet
+samarbeidet med TMII.
+Det vises ellers til Markedsføringslovens § 28 samt Straffelovens § 294.
+(Sted, dato)
+Underskrift:
       str
     end
     def partner
       return <<-str
-System's Partner Agreement
+The MadLab Institute of Innovation Norway
+Avtale om taushetsplikt
+Bedriftsnavn: _______________________ + IP/prosjekt/registreringsdata og The MadLab
+Institute of Innovation (heretter kalt ”Partene”) har blitt enige om et samarbeid om
+gjennomgang av dokumenter. I denne forbindelse har Partene inngått følgende avtale:
+1. Definisjon
+Med ”Konfidensiell Informasjon” skal i denne avtale forstås all informasjon som gis av en part
+til den annen part som enten utgjør en forretningshemmelighet, er betegnet som en
+forretningshemmelighet eller som av andre grunner er av en slik karakter at den ikke skal
+være tilgjengelig for andre. Konfidensiell informasjon omfatter, men er ikke begrenset til
+strategidokumenter, handlingsplaner, budsjetter, regnskap, tabellariske oppstillinger,
+tekniske opplysninger, økonomimodeller, erfaringstall, prognoser, immaterielle rettigheter og
+salg- og markedsføringsmetoder og -materiell.
+2. Taushetsplikt
+Hver av Partene vil motta informasjon om hverandre og den respektive parts virksomhet.
+Partene forplikter seg til å besørge at alle deres ansatte og / eller konsulenter som vil få
+tilgang til deler av eller all Konfidensiell Informasjon som omfattes av denne avtale, forut for
+arbeidets påbegynnelse blir gjort oppmerksom på denne avtale og forpliktes til å holde all
+Konfidensiell Informasjon hemmelig i samsvar med avtalen.
+3. Forbud mot urettmessig bruk
+Partene forplikter seg gjensidig til å avstå fra enhver bruk av Konfidensiell Informasjon som
+tilhører en annen part med unntak av bruk i anledning samarbeidet mellom Partene.
+4. Behandling og oppbevaring
+Partene forplikter seg til å behandle, oppbevare og holde all Konfidensiell Informasjon
+utilgjengelig for andre enn de som er parter i denne avtalen, inklusive ansatte hos Partene
+som ikke utfører arbeid i tilknytning til samarbeidet mellom Partene.
+Partene forplikter seg gjensidig til å returnere all skriftlig dokumentasjon som er mottatt fra en
+annen part og som er Konfidensiell Informasjon, etter at samarbeidet er avsluttet, eller hvis
+en part krever det.
+5. Rettigheter
+Partene er enige om at frigivelse Partene imellom av Konfidensiell Informasjon ikke
+innebærer overføring av rettigheter, herunder immaterielle rettigheter, fra en part til en
+annen.
+6. Varighet
+Taushetsplikten etter denne avtale gjelder uten tidsbegrensning, og forplikter dermed
+Partene og deres ansatte også etter opphør av samarbeidet mellom Partene.
+The MadLab Institute of Innovation Norway
+7. Begrensning
+Taushetsplikten oppheves dersom Konfidensiell Informasjon gjøres offentlig kjent av den part
+som har krav på hemmelighold, eller offentliggjøres med samtykke fra denne parten.
+Denne avtalen er utferdiget i to eksemplarer, hvorav partene beholder hvert sitt.
+Sted: Oslo
+dato:________. 2011
+For kunde For The MadLab Institute of Innovation
+
+..................................... ............
+Jakob Engebakken - Daglig leder
+
       str
     end
   end
