23bc4c0b77acadb4bb01f01fdeb347e7d6dfa839
Yen Ha Thi Bach <yencn02@gmail.com>
2012-02-21 15:02:19 +0700

    Uncomment migrations

--- db/migrate/20120220091617_document_change_project_id_to_documentable.rb
+++ db/migrate/20120220091617_document_change_project_id_to_documentable.rb
@@ -1,8 +1,8 @@
 class DocumentChangeProjectIdToDocumentable < ActiveRecord::Migration
   def up
-#    rename_column :documents, :project_id, :documentable_id
-#    add_column :documents, :documentable_type, :string
-#    Document.update_all("documentable_type = 'Project'")
+    rename_column :documents, :project_id, :documentable_id
+    add_column :documents, :documentable_type, :string
+    Document.update_all("documentable_type = 'Project'")
     add_column :projects, :use_user_avatar, :boolean
   end
 
