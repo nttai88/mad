6723a09d693f4712f74525ea9bb79f32b80a0ed0
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-27 21:34:56 +0200

    Move mailboxer gem inside vendor/plugins.

---
+++ vendor/plugins/mailboxer/app/models/conversation.rb
@@ -0,0 +1,131 @@
+class Conversation < ActiveRecord::Base
+	has_many :messages, :dependent => :destroy
+	has_many :receipts, :through => :messages
+
+	validates_presence_of :subject
+
+	before_validation :clean
+
+	scope :participant, lambda {|participant|
+    joins(:receipts).select('DISTINCT conversations.*').where('notifications.type'=> Message.to_s,'receipts.receiver_id' => participant.id,'receipts.receiver_type' => participant.class.to_s).order("conversations.updated_at DESC")
+  }
+	scope :inbox, lambda {|participant|
+    joins(:receipts).select('DISTINCT conversations.*').where('notifications.type'=> Message.to_s,'receipts.receiver_id' => participant.id,'receipts.receiver_type' => participant.class.to_s, 'receipts.mailbox_type' => 'inbox','receipts.trashed' => false).order("conversations.updated_at DESC")
+  }
+	scope :sentbox, lambda {|participant|
+    joins(:receipts).select('DISTINCT conversations.*').where('notifications.type'=> Message.to_s,'receipts.receiver_id' => participant.id,'receipts.receiver_type' => participant.class.to_s, 'receipts.mailbox_type' => 'sentbox','receipts.trashed' => false).order("conversations.updated_at DESC")
+  }
+	scope :trash, lambda {|participant|
+    joins(:receipts).select('DISTINCT conversations.*').where('notifications.type'=> Message.to_s,'receipts.receiver_id' => participant.id,'receipts.receiver_type' => participant.class.to_s,'receipts.trashed' => true).order("conversations.updated_at DESC")
+  }
+	scope :unread,  lambda {|participant|
+    joins(:receipts).select('DISTINCT conversations.*').where('notifications.type'=> Message.to_s,'receipts.receiver_id' => participant.id,'receipts.receiver_type' => participant.class.to_s,'receipts.read' => false).order("conversations.updated_at DESC")
+  }
+
+  #Mark the conversation as read for one of the participants
+	def mark_as_read(participant)
+		return if participant.nil?
+		return self.receipts_for(participant).mark_as_read
+	end
+
+  #Mark the conversation as unread for one of the participants
+	def mark_as_unread(participant)
+		return if participant.nil?
+		return self.receipts_for(participant).mark_as_unread
+	end
+
+  #Move the conversation to the trash for one of the participants
+	def move_to_trash(participant)
+		return if participant.nil?
+		return self.receipts_for(participant).move_to_trash
+	end
+
+  #Takes the conversation out of the trash for one of the participants
+	def untrash(participant)
+		return if participant.nil?
+		return self.receipts_for(participant).untrash
+	end
+
+  #Returns an array of participants
+	def recipients
+		if self.last_message
+			recps = self.last_message.recipients
+			recps = recps.is_a?(Array) ? recps : [recps]
+		return recps
+		end
+		return []
+	end
+	
+  #Returns an array of participants
+	def participants
+	  return recipients
+	end
+
+	#Originator of the conversation.
+	def originator
+		@orignator = self.original_message.sender if @originator.nil?
+		return @orignator
+	end
+
+	#First message of the conversation.
+	def original_message
+		@original_message = self.messages.find(:first, :order => 'created_at') if @original_message.nil?
+		return @original_message
+	end
+
+	#Sender of the last message.
+	def last_sender
+		@last_sender = self.last_message.sender if @last_sender.nil?
+		return @last_sender
+	end
+
+	#Last message in the conversation.
+	def last_message
+		@last_message = self.messages.find(:first, :order => 'created_at DESC') if @last_message.nil?
+		return @last_message
+	end
+
+  #Returns the receipts of the conversation for one participants
+	def receipts_for(participant)
+	  return Receipt.conversation(self).recipient(participant)
+	end
+
+  #Returns the number of messages of the conversation
+	def count_messages
+		return Message.conversation(self).count
+	end
+
+  #Returns true if the messageable is a participant of the conversation
+	def is_participant?(participant)
+		return false if participant.nil?
+		return self.receipts_for(participant).count != 0
+	end
+
+  #Returns true if the participant has at least one trashed message of the conversation
+	def is_trashed?(participant)
+		return false if participant.nil?
+		return self.receipts_for(participant).trash.count!=0
+	end
+
+  #Returns true if the participant has trashed all the messages of the conversation
+	def is_completely_trashed?(participant)
+		return false if participant.nil?
+		return self.receipts_for(participant).trash.count==self.receipts(participant).count
+	end
+
+  #Returns true if the participant has at least one unread message of the conversation
+	def is_unread?(participant)
+		return false if participant.nil?
+		return self.receipts_for(participant).unread.count!=0
+	end
+
+	protected
+
+	include ActionView::Helpers::SanitizeHelper
+
+  #Use the default sanitize to clean the conversation subject
+	def clean
+		self.subject = sanitize self.subject
+	end
+
+end
---
+++ vendor/plugins/mailboxer/mailboxer.gemspec
@@ -0,0 +1,40 @@
+Gem::Specification.new do |s|
+	s.name = "mailboxer"
+	s.version = "0.5.4"
+	s.authors = ["Eduardo Casanova Cuesta"]
+	s.summary = "Messaging system for rails apps."
+	s.description = "A Rails engine that allows any model to act as messageable, allowing it to exchange messages " +
+	                 "with any other messageable model, even different ones. It supports the use of conversations with " +
+	                 "two or more recipients to organize the messages. You have a complete use of a mailbox object for " + 
+	                 "each messageable model that manages an inbox, sentbox and trash for conversations. It also supports " + 
+	                 "sending notifications to messageable models, intended to be used as system notifications."
+	s.email = "ecasanovac@gmail.com"
+	s.homepage = "https://github.com/ging/mailboxer"
+	s.files = `git ls-files`.split("\n")
+
+	# Gem dependencies
+	#
+	# SQL foreign keys
+	s.add_runtime_dependency('foreigner', '>= 0.9.1')
+
+	# Development Gem dependencies
+	#
+  s.add_runtime_dependency('rails', '>= 3.1.0')
+  s.add_runtime_dependency('carrierwave', '>= 0.5.8')
+  s.add_runtime_dependency('sunspot_rails', '>= 1.3.0')
+	# Testing database
+	s.add_development_dependency('sqlite3-ruby')
+	# Debugging
+	if RUBY_VERSION < '1.9'
+		s.add_development_dependency('ruby-debug', '>= 0.10.3')
+	end
+	# Specs
+	s.add_development_dependency('rspec-rails', '>= 2.6.1')
+	# Fixtures
+	s.add_development_dependency('factory_girl', '>= 1.3.2')
+	# Population
+	s.add_development_dependency('forgery', '>= 0.3.6')
+	# Integration testing
+	s.add_development_dependency('capybara', '>= 0.3.9')
+end
+
