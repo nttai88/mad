db8f1c9cd3187c9a3d98a9360ea636ec96901243
Naveed Ahmad <naveed.ahmad@7vals.com>
2012-03-03 19:20:20 +0500

    add locale for english on signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   SECURE_CONTROLLERS = ["sessions", "users", "confirmations", "passwords"]
   before_filter :force_ssl
+  before_filter :set_locale
+
   protect_from_forgery
 
   include AuthenticatedSystem
@@ -16,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def force_ssl
-    if Rails.env == 'production'
+    if Rails.env.production?
       if SECURE_CONTROLLERS.include?(controller_name) && !request.ssl?
         redirect_to request.url.gsub(/^http:/, "https:")
         return false;
@@ -25,5 +27,12 @@ class ApplicationController < ActionController::Base
         redirect_to request.url.gsub(/^https:/, "http:")
       end
     end
+  end
+
+  def set_locale
+   # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = :nb#params[:locale]
+    debugger
+    p "d"
   end
 end
