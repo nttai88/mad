b5a0670765ec76bb64b1a967867339016aa7a0ff
Uģis Ozols <ugis.ozolss@gmail.com>
2012-04-03 12:23:51 +0300

    Simply use :protocol to redirect to https or http.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,10 @@ class ApplicationController < ActionController::Base
   def force_ssl
     if Rails.env.production?
       if SECURE_CONTROLLERS.include?(controller_name) && !request.ssl?
-        redirect_to request.url.gsub(/^http:/, "https:")
-        return false;
+        redirect_to :protocol => "https://"
       end
       if !SECURE_CONTROLLERS.include?(controller_name) && request.ssl?
-        redirect_to request.url.gsub(/^https:/, "http:")
+        redirect_to :protocol => "http://"
       end
     end
   end
