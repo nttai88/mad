45fd448490bc9b4ef948cda99015f987951795b3
Yen Ha Thi Bach <yencn02@gmail.com>
2012-04-12 13:38:31 +0700

    Replace Sender column with Receiver column in the Sent Box

--- app/helpers/messaging/application_helper.rb
+++ app/helpers/messaging/application_helper.rb
@@ -4,5 +4,11 @@ module Messaging
     include Refinery::MenuHelper
     include Refinery::MetaHelper
     include CommonHelpers
+
+    def receivers(m)
+      result = m.last_message.recipients.map{|x| x.to_s}.uniq
+      result.delete_if{|x| x == current_messaging_user.to_s} if result.size > 1
+      result.join(", ")
+    end
   end
 end
