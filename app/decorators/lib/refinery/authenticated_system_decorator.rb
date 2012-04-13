bcbe5ed04aa817271a18acd9565fe8ccc18d74dd
Yen Ha Thi Bach <yencn02@gmail.com>
2012-04-13 17:02:46 +0700

    Fix bug on message module

--- app/decorators/lib/refinery/authenticated_system_decorator.rb
+++ app/decorators/lib/refinery/authenticated_system_decorator.rb
@@ -1,5 +1,5 @@
 Refinery::AuthenticatedSystem.module_eval do
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || Refinery::Page.my_page_url
+    stored_location_for(resource_or_scope) || refinery.url_for(Refinery::Page.my_page_url)
   end
 end
