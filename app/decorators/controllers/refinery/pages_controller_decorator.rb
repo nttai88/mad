b07b8860e8c443d36d244112c4e36c5ad4790cb4
Yen Ha Thi Bach <yencn02@gmail.com>
2012-04-13 15:13:07 +0700

    Change url (updated)

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -5,7 +5,7 @@ Refinery::PagesController.class_eval do
   def login_required_for_my_page
     if ["my-page", "min-side"].index(params[:path])
       unless current_user
-        login_required
+        redirect_to main_app.new_refinery_user_session_path
       else
         @new_emails = Conversation.unread(current_user).count
         @projects = (current_user.admin? ? Project.count : current_user.projects.count)
