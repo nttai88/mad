a6a60d485c5320d360ac2ccfa13b1b9edfb4a939
Yen Ha Thi Bach <yencn02@gmail.com>
2012-04-13 18:40:15 +0700

    Fix ssl issue(update)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,9 +21,9 @@ class ApplicationController < ActionController::Base
       # this is a temp fix (see https://github.com/rails/journey/pull/24)
       # TODO: remove when journey 1.0.4 gets released
       if SECURE_CONTROLLERS.include?(controller_name) && !request.ssl?
-        redirect_to refinery.url_for(request.params.merge(:protocol => "https"))
+        redirect_to main_app.url_for(request.params.merge(:protocol => "https", :locale => I18n.locale))
       elsif !SECURE_CONTROLLERS.include?(controller_name) && request.ssl?
-        redirect_to refinery.url_for(request.params.merge(:protocol => "http"))
+        redirect_to main_app.url_for(request.params.merge(:protocol => "http", :locale => I18n.locale))
       end
     end
   end
